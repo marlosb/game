17c071b3fea30d14c0f4356186a81ef826db325f
MarlosB <marlosbosso@outlook.com>
2022-12-24 16:27:15 -0300

    working in progress on enemy movement

--- enemies.py
+++ enemies.py
@@ -13,35 +13,68 @@ class Enemy:
                          path[0][1] * self.game.TILES_HEIGHT + self.POSITION_OFFSET)
         self.path_position = 0
         self.size = size
-           
-    def move(self):
+        self.direction = (1,0)  
+
+    def get_direction(self):
         current_x = self.position[0]
         current_y = self.position[1]
-        next_x = self.path[self.path_position][0] * self.game.TILES_WIDTH + self.POSITION_OFFSET
-        next_y = self.path[self.path_position][1] * self.game.TILES_HEIGHT + self.POSITION_OFFSET
 
-        elapsed_time_seconds = self.game.delta_time / 1000
-        step = self.speed * elapsed_time_seconds
+        self.path_position = self.path_position + 1
+        next_x = (self.path[self.path_position][0] * self.game.TILES_WIDTH) + self.POSITION_OFFSET
+        next_y = (self.path[self.path_position][1] * self.game.TILES_HEIGHT) + self.POSITION_OFFSET
 
-        if -0.005 < (next_x - current_x) > 0.005:
-            step_x = self.speed * elapsed_time_seconds
-            current_x = current_x + step_x
+        delta_x = next_x - current_x
+        delta_y = next_y - current_y
 
-        if -0.005 < (next_y - current_y) > 0.005:
-            step_y = self.speed * elapsed_time_seconds
-            current_y = current_y + step_y
+        if (abs(delta_x) > abs(delta_y)):
+            if delta_x > 0:
+                self.direction = (1,0)
+            else:
+                self.direction = (-1,0)
+        else:
+            if delta_y > 0:
+                self.direction = (0,1)
+            else:
+                self.direction = (0,-1)
 
-        if current_x >= next_x and current_y >= next_y:
-            self.path_position = self.path_position + 1
+    def move(self):
+        elapsed_seconds = self.game.delta_time / 1000
+        step = self.speed * elapsed_seconds
+        #step = 20
 
-        return current_x, current_y
+        current_x = self.position[0] + (step * self.direction[0])
+        current_y = self.position[1] + (step * self.direction[1])
+
+        next_x = (self.path[self.path_position][0] * self.game.TILES_WIDTH) + self.POSITION_OFFSET
+        next_y = (self.path[self.path_position][1] * self.game.TILES_HEIGHT)+ self.POSITION_OFFSET
+
+        if self.direction[0]:
+            if current_x > next_x:
+                overrun = current_x - next_x
+                self.position = next_x, current_y
+                self.get_direction()
+                overrun_x = overrun * self.direction[0]
+                overrun_y = overrun * self.direction[1]
+                self.position = next_x + overrun_x, current_y + overrun_y
+            else:
+                self.position = current_x, current_y
+        else:
+            if current_y > next_y:
+                overrun = current_y - next_y
+                self.position = current_x, next_y
+                self.get_direction()
+                overrun_x = overrun * self.direction[0]
+                overrun_y = overrun * self.direction[1]
+                self.position = current_x + overrun_x, next_y + overrun_y
+            else: 
+                self.position = current_x, current_y
 
     def update(self):
-        self.position = self.move()
+        self.move()
 
     def check_oob(self): # check if it is out of boundaries
         max_x, max_y = pygame.display.get_window_size()
-        if (self.position[0] >= max_x) or (self.position[1] >= max_y):
+        if (self.position[0] + self.size >= max_x) or (self.position[1]  + self.size >= max_y):
             return True
         return False
 
