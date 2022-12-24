e594b7e40f948b134cee1d68c9ae7942fe8c5d59
MarlosB <marlosbosso@outlook.com>
2022-12-24 16:26:44 -0300

    create method order path to set the order that enemies need to follow on map

--- map.py
+++ map.py
@@ -7,10 +7,10 @@ init_map = [
     [P, P, P, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, ],
     [1, 1, P, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, ],
     [1, 1, P, P, P, P, P, P, 1, 1, 1, 1, 1, 1, 1, 1, ],
-    [1, 1, 1, 1, 1, 1, 1, P, 1, 1, 1, 1, 1, 1, 1, 1, ],
-    [1, 1, 1, 1, P, P, P, P, 1, 1, 1, 1, 1, 1, 1, 1, ],
-    [1, 1, 1, 1, P, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, ],
-    [1, 1, 1, 1, P, P, P, P, P, P, P, P, P, P, P, 1, ],
+    [1, 1, 1, 1, 1, 1, 1, P, 1, 1, 1, P, P, P, P, 1, ],
+    [1, 1, 1, 1, P, P, P, P, 1, 1, 1, P, 1, 1, P, 1, ],
+    [1, 1, 1, 1, P, 1, 1, 1, 1, 1, 1, P, 1, 1, P, 1, ],
+    [1, 1, 1, 1, P, P, P, P, P, P, P, P, 1, 1, P, 1, ],
     [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, P, 1, ],
     [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, P, 1, ],
     [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, P, 1, ]]
@@ -22,6 +22,7 @@ class Map:
         self.game = game
         self.mini_map = init_map
         self.world_map = self.get_map()
+        self.order_path()
 
     def get_map(self):
         full_map = {}
@@ -33,6 +34,35 @@ class Map:
                 else:
                     self.path.append((i,j))
         return full_map
+
+    def order_path(self):
+        ordered_path = []
+
+        for step, actual_step in enumerate(self.path):
+            if step == 0:
+                ordered_path.append(actual_step)
+                previous_step = actual_step
+                continue
+
+            # check if right tile is on path
+            candidate_tile = previous_step[0] + 1, previous_step[1]
+            if (candidate_tile in self.path) and (candidate_tile not in ordered_path):
+                next_step = candidate_tile
+            # check if bottom tile is on path
+            candidate_tile = previous_step[0], previous_step[1] + 1
+            if (candidate_tile in self.path) and (candidate_tile not in ordered_path):
+                next_step = candidate_tile
+            # check if left tile is on path
+            candidate_tile = previous_step[0] - 1, previous_step[1]
+            if (candidate_tile in self.path) and (candidate_tile not in ordered_path):
+                next_step = candidate_tile
+            # check if upp tile is on path
+            candidate_tile = previous_step[0], previous_step[1] - 1
+            if (candidate_tile in self.path) and (candidate_tile not in ordered_path):
+                next_step = candidate_tile
+            ordered_path.append(next_step)
+            previous_step = next_step         
+        self.path = ordered_path
 
     def get_rectangule(self, position):
         return (position[0] * self.tile_height, 
